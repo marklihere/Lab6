23dd1402770b58144559f51f0a4808a8b759f076
marklihere <mark.li@usu.edu>
2016-11-08 15:45:39 -0700

    Drew initial screen

--- main.c
+++ main.c
@@ -10,26 +10,70 @@ unsigned char *SYSCTL = (unsigned char *) 0x400FE000;
 unsigned char *SSI0 = (unsigned char *) 0x40008000;
 volatile unsigned int SSI0_SR __attribute__((at(0x4000800C)));
 
-void Fill_Red(void) {
-		int p,C,P;
-	
-	C = 240;
-	P = 320;
-	
-	//set column
-	writeCmd(0x2A);
-	writeDat2(0x2B);
-	writeDat2(0x6C);
-	//set page
-	writeCmd(0x2B);
-	writeDat2(0x43);
-	writeDat2(0x89);
-	
-	writeCmd(0x2C);
-	for(p = 0; p < C*P; p++)
-		writeDat2(red);
-	
+void mywriteCmd(unsigned char CMD) {
+	// wait until FIFO is not busy and also FIFO is empty
+	while((SSI0b->SR & 0x11) != 0x01);
+	GPIOB->DATA = 0x0;  // command is 0
+	SSI0b->DR = CMD;
 }
+
+void mywriteDat(unsigned char DAT) {
+	// wait until FIFO is not busy and also FIFO is empty
+	while((SSI0b->SR & 0x11) != 0x01);
+	GPIOB->DATA = 0x1;  // data is 1
+	SSI0b->DR = DAT;
+}
+
+void mywriteDat2(unsigned short DAT) {
+	// wait until FIFO is not busy and also FIFO is empty
+	while((SSI0b->SR & 0x11) != 0x01);
+	GPIOB->DATA = 0x1;  // data is 1
+	SSI0b->DR = DAT >> 8;
+	while((SSI0b->SR & 0x11) != 0x01);
+	SSI0b->DR = DAT;
+}
+
+void mywriteDat4(unsigned int DAT) {
+	// wait until FIFO is not busy and also FIFO is empty
+	while((SSI0b->SR & 0x2) == 0x0);
+	GPIOB->DATA = 0x1;  // data is 1
+	SSI0b->DR = DAT >> 24;
+	while((SSI0b->SR & 0x2) == 0x0);
+	SSI0b->DR = DAT >> 16;
+	while((SSI0b->SR & 0x2) == 0x0);
+	SSI0b->DR = DAT >> 8;
+	while((SSI0b->SR & 0x2) == 0x0);
+	SSI0b->DR = DAT;
+}
+
+void mysetArea(unsigned short x1, unsigned short x2, unsigned short y1, unsigned short y2) {
+  // Column Address Set 0x2A
+	// columns range from 0 to 239
+	mywriteCmd(0x2A);
+	mywriteDat2(x1);
+	mywriteDat2(x2);
+	
+	// Page Address Set 0x2B
+	// pages range from 0 to 319
+	mywriteCmd(0x2B);
+	mywriteDat2(y1);
+	mywriteDat2(y2);
+}
+
+
+void mywriteColor(unsigned short color) {
+	int i;
+	int cols;
+	int rows;
+	cols = 240;
+	rows = 320;
+
+  mywriteCmd(0x2C);
+	for(i = 0; i < cols*rows; i++)
+	  mywriteDat2(color);
+}
+
+
 
 int main(void)
 {
@@ -37,20 +81,27 @@ int main(void)
 	GPIO_INIT();
 	INIT_SSI0();
 	
-	// Draw_Empty_Red();
-	// Draw_Empty_Green();
-	// Draw_Empty_Yellow();
-	Fill_Red();
-	// Fill_Green();
-	// Fill_Yellow();
-	
-	//setup_SSIO_0(); //setup SPI on PA2-PA5
-	//setup_PB();     //Setup D/C on PB0
-	
 	LCD_Init();
+	
+	// draw_init();
+	mysetArea(0, 239, 0, 319);
+	mywriteColor(black);
+	mysetArea(62, 142, 40, 115);
+	mywriteColor(red);
+  mysetArea(67, 137, 45, 110);
+	mywriteColor(black);
+	mysetArea(62, 142, 123, 198);
+	mywriteColor(green);
+  mysetArea(67, 137, 128, 193);
+	mywriteColor(black);
+	mysetArea(62, 142, 206, 281);
+	mywriteColor(yellow);
+  mysetArea(67, 137, 211, 277);
+	mywriteColor(black);	
+	
     while (1)
     {
-        Fill_Red();
+      
     }
 
 }
